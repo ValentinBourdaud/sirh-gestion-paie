0947d7d4160f2404d0ea0b4eae3bc836c606652e
Valentin Bourdaud <bourdaudv@gmail.com>
2018-05-24 17:01:50 +0200

    Tests passants Tp02

---
+++ src/main/java/dev/paie/config/ServicesConfig.java
@@ -0,0 +1,12 @@
+package dev.paie.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:jdd-config.xml")
+@ComponentScan({ "dev.paie.service", "dev.paie.util" })
+public class ServicesConfig {
+
+}
